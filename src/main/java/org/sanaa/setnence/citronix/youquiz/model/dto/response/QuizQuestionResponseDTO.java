9b1a696d3223e7c489e453add69a49b475bf0c96
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-28 15:13:56 +0100

    quiz question response dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/response/QuizQuestionResponseDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/response/QuizQuestionResponseDTO.java
@@ -1,4 +1,15 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuizQuestionResponseDTO {
+    private Long id;
+    private Duration timer;
 }

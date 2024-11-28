70a1eebbf7d597d8b19f7c2303f947fa114ccb8c
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-28 23:56:21 +0100

    AnswerEmbeddedDTO

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/embedded/AnswerEmbeddedDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/embedded/AnswerEmbeddedDTO.java
@@ -1,4 +1,14 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.embedded;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnswerEmbeddedDTO {
+    private Long id;
+    private String text;
+    private boolean isCorrect;
 }

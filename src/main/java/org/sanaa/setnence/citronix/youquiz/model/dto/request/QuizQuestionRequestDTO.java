689265077b95792b21833c6d47f0671f4a337265
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 01:33:07 +0100

    quiz dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizQuestionRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizQuestionRequestDTO.java
@@ -1,5 +1,6 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import java.time.Duration;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuizQuestionRequestDTO {
+    @NotNull(message = "Timer cannot be null.")
     private Duration timer;
+    @NotNull(message= "question id can't be null")
     private long QuestionId ;
+    @NotNull(message= "quiz id can't be null")
     private long QuizId;
 }
--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizRequestDTO.java
@@ -1,5 +1,8 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +11,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuizRequestDTO {
+    @NotBlank(message = "Title cannot be blank.")
     private String title;
+    @Min(value = 0, message = "Passing score must be a non-negative value.")
     private int passingScore;
     private Boolean canSeeAnswers;
     private Boolean canSeeResult;
+    @Min(value = 0, message = "Chances must be a non-negative value.")
     private int chances;
+    @NotNull
     private String remark;
+    @NotNull
     private Long trainerId;
 }

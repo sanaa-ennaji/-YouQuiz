318cb891ed90bbdb9533f586c0776b4fb61801d8
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 11:54:02 +0100

    subject response dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/SubjectServiceI.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/SubjectServiceI.java
@@ -1,4 +1,8 @@
 package org.sanaa.setnence.citronix.youquiz.service.interfaces;
 
-public interface SubjectServiceI {
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.SubjectRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.SubjectResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Subject;
+
+public interface SubjectServiceI extends GenericServiceI<Subject, SubjectRequestDTO, SubjectResponseDTO>{
 }

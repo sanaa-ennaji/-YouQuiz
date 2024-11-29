cdf36262bdfd569a22b674720ecb4e484ce9abe5
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 02:18:54 +0100

    Quiz mapper

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/QuizMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/QuizMapper.java
@@ -1,4 +1,16 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.QuizRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.QuizResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Quiz;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
 public interface QuizMapper {
+    QuizResponseDTO toResponseDTO(Quiz entity);
+    Quiz toEntity (QuizRequestDTO requestDTO);
+    List<QuizResponseDTO> toResponseDTOList(List<Quiz> entities);
+    List<Quiz> toEntityList(List<QuizResponseDTO> requestDTOs);
 }

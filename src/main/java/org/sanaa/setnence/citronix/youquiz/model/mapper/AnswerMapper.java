055c8413dd8dac80546108673508dd8867ef121b
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 01:52:35 +0100

    answer dto interface

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerMapper.java
@@ -1,4 +1,16 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
-public class AnswerMapper {
+import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.AnswerRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.AnswerResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Answer;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
+public interface AnswerMapper {
+    AnswerResponseDTO toResponseDTO(Answer entity) ;
+    Answer toEntity(AnswerRequestDTO requestDTO);
+    List<AnswerResponseDTO> toResponseDTOList (List<Answer> entities);
+    List<Answer> toEntityList(List<AnswerRequestDTO> requestDTOs);
 }
--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerValidationMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerValidationMapper.java
@@ -1,4 +1,4 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
-public class AnswerValidationMapper {
+public interface AnswerValidationMapper {
 }

030d62c2558abc5d90e4609287d4b91b00369eee
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 01:18:04 +0100

    level Request dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/AnswerValidationRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/AnswerValidationRequestDTO.java
@@ -1,6 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,7 @@ public class AnswerValidationRequestDTO {
     private Long id;
     @Min(value = 0, message = "Points must be a non-negative value.")
     private  int points;
+    @Positive(message = "Question ID must be a positive value.")
+    private long questionId;
 
 }
--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/LevelRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/LevelRequestDTO.java
@@ -1,5 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LevelRequestDTO {
+
+    @NotBlank(message = "Description cannot be blank.")
     private String description;
+    @Min(value = 0, message = "Min points must be a non-negative value.")
     private int minPoints;
+    @Min(value = 0, message = "Max points must be a non-negative value.")
     private int maxPoints;
 }

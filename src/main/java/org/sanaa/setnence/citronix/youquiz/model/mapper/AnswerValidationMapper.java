2e687d5e6c21ce591edefcb53e8d72f712014116
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 02:08:04 +0100

    answer validation

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerValidationMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/AnswerValidationMapper.java
@@ -1,10 +1,19 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.AnswerValidationRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.AnswerValidationResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.AnswerValidation;
+
+
+import java.util.List;
 
 
 @Mapper(componentModel = "spring", uses = {AnswerMapper.class})
 public interface AnswerValidationMapper {
-
+    AnswerValidationResponseDTO toResponseDTO(AnswerValidation entity);
+    AnswerValidation toEntity (AnswerValidationRequestDTO requestDTO);
+    List<AnswerValidationResponseDTO> toResponseDTOList(List<AnswerValidation> entities);
+    List<AnswerValidation> toEntityList(List<AnswerValidationResponseDTO> requestDTOs);
 
 }

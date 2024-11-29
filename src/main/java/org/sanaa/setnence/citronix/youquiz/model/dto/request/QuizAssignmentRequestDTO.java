7ef1ccafc06aa884de994291c104dc93ae7cba2d
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 01:28:00 +0100

    quiz question dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizAssignmentRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuizAssignmentRequestDTO.java
@@ -1,5 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuizAssignmentRequestDTO {
+    @NotBlank(message = "Reason cannot be blank.")
     private String reason;
+    @NotNull(message = "Start date cannot be null.")
     private LocalDate startDate;
+    @NotNull(message = "End date cannot be null.")
     private LocalDate endDate;
+    @NotNull(message = "student id can't be null")
     private Long studentId;
 }

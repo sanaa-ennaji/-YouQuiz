95fe8d8c026f332be4947b52523ad222ab04d365
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 11:21:06 +0100

    level repo

--- src/main/java/org/sanaa/setnence/citronix/youquiz/repository/LevelRepository.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/repository/LevelRepository.java
@@ -1,4 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.repository;
 
-public interface LevelRepository {
+import org.sanaa.setnence.citronix.youquiz.model.entity.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
 }

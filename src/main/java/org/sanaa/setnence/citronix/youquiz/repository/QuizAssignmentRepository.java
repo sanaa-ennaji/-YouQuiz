d7374e92c29160e1636170fccac5806953b2e9c9
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 11:23:34 +0100

    QuizAssignment

--- src/main/java/org/sanaa/setnence/citronix/youquiz/repository/QuizAssignmentRepository.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/repository/QuizAssignmentRepository.java
@@ -1,4 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.repository;
 
-public interface QuizAssignmentRepository {
+import org.sanaa.setnence.citronix.youquiz.model.entity.QuizAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizAssignmentRepository extends JpaRepository<QuizAssignment, Long> {
 }

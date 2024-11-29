f76731794f3a7ca550923a87b58ec2735e43a0da
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 02:06:39 +0100

    level mapper

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/LevelMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/LevelMapper.java
@@ -1,8 +1,18 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.LevelRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.LevelResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Level;
+
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {QuestionMapper.class, AnswerMapper.class})
 public interface LevelMapper {
+    LevelResponseDTO toResponseDTO(Level entity);
+    Level toEntity (LevelRequestDTO requestDTO);
+    List<LevelResponseDTO> toResponseDTOList(List<Level> entities);
+    List<Level> toEntityList(List<LevelResponseDTO> requestDTOs);
 
 }

4a99f68dc134bbdfb8ef1d59ab7cdddb67c14076
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 14:48:35 +0100

    levele service interface

--- src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/LevelServiceI.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/LevelServiceI.java
@@ -1,4 +1,8 @@
 package org.sanaa.setnence.citronix.youquiz.service.interfaces;
 
-public interface LevelServiceI {
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.LevelRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.LevelResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Level;
+
+public interface LevelServiceI extends GenericServiceI<Level, LevelRequestDTO, LevelResponseDTO>{
 }

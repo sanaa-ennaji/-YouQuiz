2c1fef56e2173b8f87458bbaa795bc574b55125f
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 14:57:57 +0100

    QuizQuestion service interface

--- src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/QuizQuestionServiceI.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/service/interfaces/QuizQuestionServiceI.java
@@ -1,4 +1,8 @@
 package org.sanaa.setnence.citronix.youquiz.service.interfaces;
 
-public interface QuizQuestionServiceI {
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.QuizQuestionRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.QuizQuestionResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.QuizQuestion;
+
+public interface QuizQuestionServiceI extends GenericServiceI<QuizQuestion, QuizQuestionRequestDTO, QuizQuestionResponseDTO>{
 }

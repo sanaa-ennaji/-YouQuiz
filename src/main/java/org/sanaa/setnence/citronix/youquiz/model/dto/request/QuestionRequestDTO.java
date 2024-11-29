6cc4031c8e685669c554598661c0fd881592b94d
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 01:25:47 +0100

    question dto

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuestionRequestDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/dto/request/QuestionRequestDTO.java
@@ -1,5 +1,8 @@
 package org.sanaa.setnence.citronix.youquiz.model.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,15 @@ import org.sanaa.setnence.citronix.youquiz.model.enums.QuestionType;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuestionRequestDTO {
+    @NotBlank(message = "Question text cannot be blank.")
     private String text;
+    @NotNull(message = "Question type cannot be null.")
     private QuestionType type;
+    @Min(value = 0, message = "Correct answers must be a non-negative value.")
     private int correctAnswers;
+
+    @NotNull
     private Long subjectId;
+    @NotNull
     private Long levelId;
 }

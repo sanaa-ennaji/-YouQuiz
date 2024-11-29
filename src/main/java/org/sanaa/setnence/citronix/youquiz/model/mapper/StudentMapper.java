bd80ac5edbbb6677aad4f222bdd5dcee15da7951
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 02:23:26 +0100

    studdent mapper

--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/StudentMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/StudentMapper.java
@@ -1,4 +1,17 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.StudentRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.StudentResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.entity.Student;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
 public interface StudentMapper {
+
+    StudentResponseDTO toResponseDTO(Student entity);
+    Student toEntity (StudentRequestDTO requestDTO);
+    List<StudentResponseDTO> toResponseDTOList(List<Student> entities);
+    List<Student> toEntityList(List<StudentResponseDTO> requestDTOs);
 }
--- src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/SubjectMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/model/mapper/SubjectMapper.java
@@ -1,4 +1,10 @@
 package org.sanaa.setnence.citronix.youquiz.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.SubjectResponseDTO;
+
+@Mapper(componentModel = "spring" )
 public interface SubjectMapper {
+
+    SubjectResponseDTO
 }

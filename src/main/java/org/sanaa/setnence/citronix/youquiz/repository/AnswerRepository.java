6a28078955e32866a8750f925c61333f2e4610ab
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-29 11:19:31 +0100

    anwer repository

--- src/main/java/org/sanaa/setnence/citronix/youquiz/repository/AnswerRepository.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/repository/AnswerRepository.java
@@ -1,4 +1,7 @@
 package org.sanaa.setnence.citronix.youquiz.repository;
 
-public interface AnswerRepository {
+import org.sanaa.setnence.citronix.youquiz.model.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }

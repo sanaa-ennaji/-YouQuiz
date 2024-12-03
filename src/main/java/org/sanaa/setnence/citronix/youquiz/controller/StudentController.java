a2356e54bd031173fe3bda0cc9784622963d98ad
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-03 17:05:24 +0100

    controller student done

--- src/main/java/org/sanaa/setnence/citronix/youquiz/controller/StudentController.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/controller/StudentController.java
@@ -1,4 +1,55 @@
 package org.sanaa.setnence.citronix.youquiz.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.StudentRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.StudentResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.service.impl.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@Validated
+@RequiredArgsConstructor
 public class StudentController {
+    private final StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<StudentResponseDTO> createStudent(@RequestBody @Valid StudentRequestDTO studentRequestDTO) {
+        StudentResponseDTO response = studentService.create(studentRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable Long id) {
+        return studentService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable Long id,
+                                                            @RequestBody @Valid StudentRequestDTO studentRequestDTO) {
+        StudentResponseDTO updatedStudent = studentService.update(id, studentRequestDTO);
+        return ResponseEntity.ok(updatedStudent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponseDTO>> getAllStudents() {
+        List<StudentResponseDTO> students = studentService.findAll();
+        return ResponseEntity.ok(students);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
+        studentService.delete(id);
+        return ResponseEntity.ok("student deletion done");
+    }
+
 }

c8931c16556f1d0a298f75a2f20af9713b4c7166
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-04 00:14:19 +0100

    subject controller

--- src/main/java/org/sanaa/setnence/citronix/youquiz/controller/SubjectController.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/controller/SubjectController.java
@@ -1,6 +1,59 @@
 package org.sanaa.setnence.citronix.youquiz.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.SubjectRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.SubjectResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.service.impl.SubjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/subjects")
+@Validated
+@RequiredArgsConstructor
 public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @PostMapping
+    public ResponseEntity<SubjectResponseDTO> createSubject(@RequestBody @Valid SubjectRequestDTO requestDTO) {
+        SubjectResponseDTO responseDTO = subjectService.create(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubjectResponseDTO> getSubjectById(@PathVariable Long id) {
+        return subjectService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SubjectResponseDTO> updateSubject(@PathVariable Long id,
+                                                            @RequestBody @Valid SubjectRequestDTO requestDTO) {
+        SubjectResponseDTO updatedSubject = subjectService.update(id, requestDTO);
+        return ResponseEntity.ok(updatedSubject);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSubject(@PathVariable Long id) {
+        subjectService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<SubjectResponseDTO>> getAllSubjects() {
+        List<SubjectResponseDTO> subjects = subjectService.findAll();
+        return ResponseEntity.ok(subjects);
+    }
 
 
 }

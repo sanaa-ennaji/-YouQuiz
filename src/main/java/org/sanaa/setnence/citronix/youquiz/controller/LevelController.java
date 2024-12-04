860c22c104026596d9d4dffdbdd9a1d27c67b2bc
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-04 17:28:08 +0100

    level and question answer done

--- src/main/java/org/sanaa/setnence/citronix/youquiz/controller/LevelController.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/controller/LevelController.java
@@ -1,13 +1,56 @@
 package org.sanaa.setnence.citronix.youquiz.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.AnswerRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.LevelRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.AnswerResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.LevelResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.service.impl.AnswerService;
+import org.sanaa.setnence.citronix.youquiz.service.impl.LevelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Level")
+@RequestMapping("/api/v1/Levels")
 @Validated
 @RequiredArgsConstructor
 public class LevelController {
+    private final LevelService levelService;
+
+    @PostMapping
+    public ResponseEntity<LevelResponseDTO> create(@Valid @RequestBody LevelRequestDTO levelRequestDTO) {
+        LevelResponseDTO levelResponse = levelService.create(levelRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(levelResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LevelResponseDTO> update(@PathVariable Long id, @Valid @RequestBody LevelRequestDTO levelRequestDTO) {
+        LevelResponseDTO updatedLevel = levelService.update(id, levelRequestDTO);
+        return ResponseEntity.ok(updatedLevel);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LevelResponseDTO> findById(@PathVariable Long id) {
+        return levelService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "level not found"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LevelResponseDTO>> findAll() {
+        List<LevelResponseDTO> levels = levelService.findAll();
+        return ResponseEntity.ok(levels);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        levelService.delete(id);
+        return ResponseEntity.ok("Level was deleted");
+    }
 }
--- src/main/java/org/sanaa/setnence/citronix/youquiz/controller/QuestionAnswerController.java
+++ src/main/java/org/sanaa/setnence/citronix/youquiz/controller/QuestionAnswerController.java
@@ -1,4 +1,48 @@
 package org.sanaa.setnence.citronix.youquiz.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.sanaa.setnence.citronix.youquiz.model.dto.request.QuestionAnswerRequestDTO;
+import org.sanaa.setnence.citronix.youquiz.model.dto.response.QuestionAnswerResponseDTO;
+import org.sanaa.setnence.citronix.youquiz.service.impl.QuestionAnswerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/questions_answers")
+@Validated
+@RequiredArgsConstructor
 public class QuestionAnswerController {
+    private final QuestionAnswerService questionAnswerService;
+
+    @PostMapping
+    public ResponseEntity<QuestionAnswerResponseDTO> create(@Valid @RequestBody QuestionAnswerRequestDTO questionAnswerRequestDTO) {
+        QuestionAnswerResponseDTO questionAnswerResponse = questionAnswerService.create(questionAnswerRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(questionAnswerResponse);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionAnswerResponseDTO> findById(@PathVariable Long id) {
+        return questionAnswerService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "QuestionAnswer not found"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuestionAnswerResponseDTO>> findAll() {
+        List<QuestionAnswerResponseDTO> quizzes = questionAnswerService.findAll();
+        return ResponseEntity.ok(quizzes);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        questionAnswerService.delete(id);
+        return ResponseEntity.ok("QuestionAnswer was deleted");
+    }
 }
